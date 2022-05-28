432b1df37aaec860e91db51e51f8cb336434c634
MyMiDiII <mymidi.student@yandex.ru>
2022-05-28 09:21:34 +0300

    FIX: lab04 codestyle

--- lab04/src/main.py
+++ lab04/src/main.py
@@ -4,9 +4,9 @@ import numpy as np
 from math import fabs, sin, pi
 from mpl_toolkits.mplot3d import Axes3D
 from data import data
+from prettytable import PrettyTable
 
 ti_max = 500
-
 
 class Function:
     def __init__(self):
@@ -33,7 +33,8 @@ class Function:
         self.tmax = data['tmax']
 
         self.h = data['h']
-        self.EPS = 1e-4
+        self.XEPS = 1
+        self.TEPS = 1e-4
 
 
     def alpha(self, x):
@@ -47,14 +48,16 @@ class Function:
 
 
     def k(self, T):
-        return self.k0 * ((T / 300)**2)
+        return self.k0 * ((T / 300) ** 2)
 
 
-    def lamda(self, T):
-        return self.a1 * (self.b1 + self.c1 * T**self.m1)
+    def Lambda(self, T):
+        return self.a1 * (self.b1 + self.c1 * T ** self.m1)
 
-    def c(self, T):
+
+    def C(self, T):
         return self.a2 + self.b2 * T ** self.m2 - (self.c2 / T ** 2)
+
 
     def f2(self, x):
         return 2 * self.T0 * self.alpha(x) / self.R
@@ -64,105 +67,113 @@ class Function:
         period = data['period']
         tu = data['tu']
         #print(t)
-        #return 10
-        print(t)
-        print(100 * fabs(sin(100 * pi * t)))
-        return 100 * fabs(sin(100 * pi * t))
+        #print(100 * fabs(sin(100 * pi * t)))
+        #return 100 * fabs(sin(100 * pi * t))
         #if (t % period <= tu):
             #return self.Fmax / self.tmax * t * e**(-(t / self.tmax - 1))
         #return self.Fmax / self.tmax * t * e**(-(t / self.tmax - 1))
-        # return 10
+        return 10
 
 
 class Lab4(Function):
-    def func_plus_half(self, x, h, func):
-        return (func(x) + func(x + h)) / 2
-
-    def func_minus_half(self, x, h, func):
-        return (func(x) + func(x - h)) / 2
-
-
     def A(self, T, n):
-        return self.tau * (self.lamda(T[n]) + self.lamda(T[n - 1])) / self.h / 2
+        return (self.tau * (self.Lambda(T[n]) + self.Lambda(T[n - 1]))
+                    / self.h / 2)
 
     def D(self, T, n):
-        return self.tau * (self.lamda(T[n]) + self.lamda(T[n + 1])) / self.h / 2
+        print(n)
+        return (self.tau * (self.Lambda(T[n]) + self.Lambda(T[n + 1]))
+                    / self.h / 2)
 
     def B(self, x, T, n):
-        return self.A(T, n) + self.D(T, n) + self.c(T[n]) * self.h + self.p(x) * self.tau * self.h
+        return (self.A(T, n) + self.D(T, n) + self.C(T[n]) * self.h +
+                    self.p(x) * self.tau * self.h)
 
     def G(self, x, T, n, ti):
-        g1 = self.c(T[n]) * self.h * T[n]
-        g2 = self.h * self.tau * self.k(T[n]) * self.F0(ti) * e**(-self.k(T[n]) * x)
+        g1 = self.C(T[n]) * self.h * T[n]
+        g2 = (self.h * self.tau * self.k(T[n])
+                    * self.F0(ti) * e ** (-self.k(T[n]) * x))
         g3 = self.f2(x) * self.h * self.tau
+
         return g1 + g2 + g3
 
-    def left_bc(self, T_prev, time):
+    def leftBC(self, Tprev, time):
         h = self.h
-        k1 = h * (self.c(T_prev[0]) + self.c(T_prev[1])) / 16
-        k2 = h * self.c(T_prev[0]) / 4
-        k3 = self.tau * (self.lamda(T_prev[0]) + self.lamda(T_prev[1])) / h / 2
+        k1 = h * (self.C(Tprev[0]) + self.C(Tprev[1])) / 16
+        k2 = h * self.C(Tprev[0]) / 4
+        k3 = self.tau * (self.Lambda(Tprev[0]) + self.Lambda(Tprev[1])) / h / 2
         k4 = self.tau * h * (self.p(0) + self.p(h)) / 16
         k5 = self.tau * h * self.p(0) / 4
         k6 = self.alpha0 * self.tau
         K0 = k1 + k2 + k3 + k4 + k5 + k6
 
-        m1 = h * (self.c(T_prev[0]) + self.c(T_prev[1])) / 16
-        m2 = self.tau * (self.lamda(T_prev[0]) + self.lamda(T_prev[1])) / h / 2
+        m1 = h * (self.C(Tprev[0]) + self.C(Tprev[1])) / 16
+        m2 = self.tau * (self.Lambda(Tprev[0]) + self.Lambda(Tprev[1])) / h / 2
         m3 = self.tau * h * (self.p(0) + self.p(h)) / 16
         M0 = m1 - m2 + m3
 
-        p1 = h * (self.c(T_prev[0]) + self.c(T_prev[1])) * (T_prev[0] + T_prev[1]) / 16
-        p2 = h * self.c(T_prev[0]) * T_prev[0] / 4
+        p1 = (h * (self.C(Tprev[0]) + self.C(Tprev[1]))
+                * (Tprev[0] + Tprev[1]) / 16)
+        p2 = h * self.C(Tprev[0]) * Tprev[0] / 4
         p3 = self.alpha0 * self.T0 * self.tau
-        p4 = 2 * self.T0 / 2 / self.R * (3 * self.alpha0 + self.alpha(h)) * h * self.tau / 4
-        p5 = h * self.tau * self.F0(time) * self.k(T_prev[0]) * e**(-self.k(T_prev[0]) * 0) / 4
-
-        p6 = h * self.tau * self.F0(time) * (self.k(T_prev[0]) + self.k(T_prev[1])) / 2 * e**(-(self.k(T_prev[0]) + self.k(T_prev[1])) / 2 * (0 + h / 2)) / 4
+        p4 = (self.T0 / self.R
+                * (3 * self.alpha0 + self.alpha(h)) * h * self.tau / 4)
+        p5 = (h * self.tau * self.F0(time) * self.k(Tprev[0]) / 4)
+        p6 = (h * self.tau * self.F0(time) * (self.k(Tprev[0]) +
+                self.k(Tprev[1])) * e**(-(self.k(Tprev[0]) +
+                    self.k(Tprev[1])) * (h / 2)) / 16)
 
         P0 = p1 + p2 + p3 + p4 + p5 + p6
 
         return K0, M0, P0
 
-    def right_bc(self, T_prev, time):
+    def rightBC(self, Tprev, time):
         h = self.h
-        k1 = h * self.c(T_prev[-1]) / 4
-        k2 = h * (self.c(T_prev[-1]) + self.c(T_prev[-2])) / 16
-        k3 = self.tau * (self.lamda(T_prev[-1]) + self.lamda(T_prev[-2])) / h / 2
+        k1 = h * self.C(Tprev[-1]) / 4
+        k2 = h * (self.C(Tprev[-1]) + self.C(Tprev[-2])) / 16
+        k3 = (self.tau * (self.Lambda(Tprev[-1]) + self.Lambda(Tprev[-2]))
+                / h / 2)
         k4 = self.tau * h * self.p(self.l) / 4
         k5 = self.tau * h * (self.p(self.l) + self.p(self.l - h)) / 16
         k6 = - self.alphaN * self.tau
         KN = k1 + k2 + k3 + k4 + k5 + k6
 
-        m1 = h * (self.c(T_prev[-1]) + self.c(T_prev[-2])) / 16
-        m2 = self.tau * (self.lamda(T_prev[-1]) + self.lamda(T_prev[-2])) / h / 2
+        m1 = h * (self.C(Tprev[-1]) + self.C(Tprev[-2])) / 16
+        m2 = (self.tau * (self.Lambda(Tprev[-1]) + self.Lambda(Tprev[-2]))
+                / h / 2)
         m3 = self.tau * h * (self.p(self.l) + self.p(self.l - h)) / 16
         MN = m1 - m2 + m3
 
-        p1 = h * (self.c(T_prev[-1]) + self.c(T_prev[-2])) / 2 * (T_prev[-1] +
-                T_prev[-2]) / 8
+        p1 = (h * (self.C(Tprev[-1]) + self.C(Tprev[-2]))
+                * (Tprev[-1] + Tprev[-2]) / 16)
         p2 = - self.tau * self.alphaN * self.T0
         p3 = self.tau * h * (3 * self.f2(self.l) + self.f2(self.l - h)) / 8
-        p4 = h * self.c(T_prev[-1]) * T_prev[-1] / 4
-        p5 = (h * self.tau * self.F0(time) * self.k(T_prev[-1])
-                * e ** (-self.k(T_prev[-1]) * self.l) / 4)
-        p6 = (h * self.tau * self.F0(time) * (self.k(T_prev[-1]) +
-                self.k(T_prev[-2])) / 2 * e**(-(self.k(T_prev[-1]) +
-                    self.k(T_prev[-2])) / 2 * (self.l - h / 2)) / 4)
+        p4 = h * self.C(Tprev[-1]) * Tprev[-1] / 4
+        p5 = (h * self.tau * self.F0(time) * self.k(Tprev[-1])
+                * e ** (-self.k(Tprev[-1]) * self.l) / 4)
+        p6 = (h * self.tau * self.F0(time) * (self.k(Tprev[-1]) +
+                self.k(Tprev[-2])) * e**(-(self.k(Tprev[-1]) +
+                    self.k(Tprev[-2])) * (self.l - h / 2)) / 16)
         PN = p1 + p2 + p3 + p4 + p5 + p6
 
         return KN, MN, PN
 
 
-    def run(self, T_prev, K0, M0, P0, KN, MN, PN, ti):
+    def run(self, Tprev, ti):
         h = self.h
+        K0, M0, P0 = self.leftBC(Tprev, ti)
+        KN, MN, PN = self.rightBC(Tprev, ti)
+
         eps = [0, -M0 / K0]
         eta = [0, P0 / K0]
         x = h
         n = 1
-        while (x + h < self.l):
-            eps.append(self.D(T_prev, n) / (self.B(x, T_prev, n) - self.A(T_prev, n) * eps[n]))
-            eta.append((self.G(x, T_prev, n, ti) + self.A(T_prev, n) * eta[n]) / (self.B(x, T_prev, n) - self.A(T_prev, n) * eps[n]))
+
+        while (x < self.l - h / 2):
+            eps.append(self.D(Tprev, n) / (self.B(x, Tprev, n)
+                           - self.A(Tprev, n) * eps[n]))
+            eta.append((self.G(x, Tprev, n, ti) + self.A(Tprev, n) * eta[n])
+                        / (self.B(x, Tprev, n) - self.A(Tprev, n) * eps[n]))
             n += 1
             x += h
 
@@ -175,50 +186,125 @@ class Lab4(Function):
         return y
 
 
+class Table(PrettyTable):
+    def add_columns(self, colDict : dict):
+        for key, value in colDict.items():
+            self.add_column(key, value[0], align="r")
+            self.custom_format[key] = value[1]
+
+
+class Graphics:
+    def __init__(self, graphics=[]):
+        self.graphics = graphics
+        self.num = len(graphics)
+
+
+    def addGraphiC(self, xVals, yVals, labels, title):
+        self.graphics.append({
+                        "x" : xVals,
+                        "y" : yVals,
+                        "labels" : labels,
+                        "title" : title
+                        })
+        self.num += 1
+
+
+    def show(self):
+        for i, graph in enumerate(self.graphics):
+            plt.subplot(2, self.num // 2 + self.num % 2, i + 1)
+
+            for j, y in enumerate(graph["y"]):
+                plt.plot(graph["x"], y, label=graph["labels"][j])
+
+            plt.legend()
+            plt.title(graph["title"])
+            plt.grid()
+
+        plt.subplots_adjust(wspace=0.3, hspace=0.3)
+        plt.show()
+
+
 class Res(Lab4):
     def res(self):
         h = self.h
-        step1 = int(self.l / h)
-        T = [self.T0] * (step1 + 1)
+        lenTx = int(self.l / h)
+        T = [self.T0] * (lenTx + 1)
 
         ti = 0
         res = []
         res.append(T)
 
-        ff = 0
         while True:
             prev = T
             ti += self.tau
-            while True:
-                K0, M0, P0 = self.left_bc(prev, ti)
-                KN, MN, PN = self.right_bc(prev, ti)
 
-                T_new = self.run(prev, K0, M0, P0, KN, MN, PN, ti)
+            diffByTMax = self.XEPS + h
+            while diffByTMax > self.XEPS:
+                Tnew = self.run(prev, ti)
+                diffByTMax = abs((T[0] - Tnew[0]) / Tnew[0])
 
-                max = (T[0] - T_new[0]) / T_new[0]
-                max = abs(max)
-                for step2, j in zip(T, T_new):
-                    d = (step2 - j) / j
-                    d = abs(d)
-                    if d > max:
-                        max = d
-                if max < 1:
-                    break
+                for curTPrev, curTNew in zip(T, Tnew):
+                    curDiff = abs((curTPrev - curTNew) / curTNew)
 
-                prev = T_new
-            res.append(T_new)
-            ff += 1
+                    if curDiff > diffByTMax:
+                        diffByTMax = curDiff
+
+                prev = Tnew
+
+            res.append(Tnew)
 
             check_eps = 0
-            for i, j in zip(T, T_new):
-                tttt = fabs((i - j) / j)
-                if fabs((i - j) / j) > self.EPS:
+            for i, j in zip(T, Tnew):
+
+                if fabs((i - j) / j) > self.TEPS:
                     check_eps = 1
+
             if check_eps == 0:
                 break
-            T = T_new
+
+            T = Tnew
 
         return res, ti
+        #h = self.h
+        #step1 = int(self.l / h) + 1
+        #T = [self.T0] * (step1 + 1)
+
+        #ti = 0
+        #res = []
+        #res.append(T)
+
+        #ff = 0
+        #while True:
+        #    prev = T
+        #    ti += self.tau
+
+        #    diffTByMax = self.EPS + h
+        #    while diffTByMax > self.EPS:
+        #        Tnew = self.run(prev, ti)
+
+        #        diffTByMax = abs((T[0] - Tnew[0]) / Tnew[0])
+
+        #        for curTPrev, curTnew in zip(T, Tnew):
+        #            curDiff = abs((curTPrev - curTnew) / curTnew)
+
+        #            if curDiff > diffTByMax:
+        #                diffTByMax = curDiff
+
+        #        prev = Tnew
+
+        #    res.append(Tnew)
+        #    ff += 1
+
+        #    check_eps = 0
+        #    for i, j in zip(T, Tnew):
+        #        tttt = fabs((i - j) / j)
+        #        if fabs((i - j) / j) > self.EPS:
+        #            check_eps = 1
+        #    if check_eps == 0:
+        #        break
+        #    T = Tnew
+
+        #return res, ti
 
 
     def draw(self):
@@ -287,7 +373,7 @@ class Res(Lab4):
 
             n = 0
             for temp in res:
-                if (fabs(n - 1) < self.EPS):
+                if (fabs(n - 1) < self.TEPS):
                     print()
                     print(self.tau)
                     print()
@@ -368,7 +454,7 @@ class Res(Lab4):
 def main():
     resh = Res()
     print(resh.draw())
-    resh.chast()
+    # resh.chast()
 
 if __name__ == "__main__":
     main()
