774a7cfdec8463012a45db4963185c681b5a3a02
MyMiDiII <mymidi.student@yandex.ru>
2022-10-06 14:37:52 +0300

    FIX: normal distribution / 2 error

--- sem07/lab01/src/distributions.py
+++ sem07/lab01/src/distributions.py
@@ -20,5 +20,5 @@ def NormalDensityFunc(x: float, m: float, sigma: float) -> float:
 
 
 def NormalDistributionFunc(x: float, m: float, sigma: float) -> float:
-    return (1 + erf((x - m) / sigma / sqrt(2))
+    return ((1 + erf((x - m) / sigma / sqrt(2))) / 2
             if sigma > 0 else -1)
