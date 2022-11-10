0bae09efc2fb5f63b83b43150a04ac2f44260f6b
MyMiDiII <mymidi.student@yandex.ru>
2022-11-10 15:27:42 +0300

    ADD: lab73 interface

---
+++ sem07/lab03/src/main.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5 import QtWidgets
+
+from gui.mainwindow import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
